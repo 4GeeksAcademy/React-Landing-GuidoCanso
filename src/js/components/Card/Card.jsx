b03a8d95528eaec25e4af23e58b4b147a5b4eac2
GuidoCanso <guidocansobre@gmail.com>
2025-03-11 18:09:31 +0000

    Arreglas realizados

--- src/js/components/Card/Card.jsx
+++ src/js/components/Card/Card.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Card = () => {
   return (
     <div className="card m-2" style={{ width: "18rem" }}>
-      <img src="https://via.placeholder.com/150" className="card-img-top" alt="..." />
+      <img src="holder.js/100px180" className="card-img-top" alt="..." />
       <div className="card-body text-center">
         <h5 className="card-title">Título</h5>
         <p className="card-text">Descripción de la tarjeta.</p>
--- src/js/components/Navbar/Navbar.jsx
+++ src/js/components/Navbar/Navbar.jsx
@@ -1,21 +1,44 @@
 import React from "react";
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+// import Container from 'react-bootstrap/Container';
+// import Nav from 'react-bootstrap/Nav';
+// import Navbar from 'react-bootstrap/Navbar';
 
-function ColorSchemesExample() {
+const NavbarComponent = () => {
     return (
-        <Navbar bg="dark" data-bs-theme="dark">
-            <Container>
-                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-                <Nav className="me-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#features">Features</Nav.Link>
-                    <Nav.Link href="#pricing">Pricing</Nav.Link>
-                </Nav>
-            </Container>
-        </Navbar>
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark w-100">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">Start Bootstrap</a>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav ms-auto">
+                        <li className="nav-item">
+                            <a className="nav-link active" href="#">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">About</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Services</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     );
 }
 
-export default ColorSchemesExample;
+export default NavbarComponent;
+
